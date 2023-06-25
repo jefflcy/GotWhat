156958f3916201557c17f49550598daa954bb35e
Jeff Lee <jefferson0310@gmail.com>
2023-06-26 03:57:51 +0800

    Minor fix

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export default function SideMenu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,20 +9,30 @@ export default function SideMenu() {
   };
 
   return (
-    <div className="pt-10 px-5">
-
-      <div onClick={toggleMenu} className='block'>
+    <div className="pt-10 px-5 relative">
+      <div onClick={toggleMenu} className="block text-white">
         {isOpen ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
-    <div className={isOpen ? 'fixed right-0 top-0 w-[20%] h-full border-r text-white border-r-gray-900 bg-[#050605] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-      <ul className='pt-24 uppercase p-4'>
-        <li className='p-4 border-b border-grey-600'><a href="/home">Home</a></li>
-        <li className='p-4 border-b border-grey-600'><a href="/login">Login</a></li>
-        <li className='p-4 '><a href="#">Request</a></li>
-      </ul>
+      <div
+        className={
+          isOpen
+            ? "fixed right-0 top-0 w-[20%] h-full border-r text-black border-r-gray-900 bg-white ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
+        <ul className="pt-24 uppercase p-4">
+          <li className="p-4 border-b border-black">
+            <a href="/home">Home</a>
+          </li>
+          <li className="p-4 border-b border-black">
+            <a href="/login">Login</a>
+          </li>
+          <li className="p-4 ">
+            <a href="#">Request</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    
-  </div>
   );
 }
