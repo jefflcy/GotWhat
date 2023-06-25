de99f6c6bd242ccf99139f722af54384a6e890fd
aseah15 <e0958666@u.nus.edu>
2023-06-26 04:56:32 +0800

    too laze to go through w this for now

--- client/src/pages/RequestPage.js
+++ client/src/pages/RequestPage.js
@@ -3,7 +3,11 @@ import SideMenu from "../components/SideMenu";
 const RequestPage = () => {
   return (
     <div>
-        
+        <SideMenu />
+        <h1>Request Form</h1>
+        <div className="">
+
+        </div>
     </div>
   )
 }
