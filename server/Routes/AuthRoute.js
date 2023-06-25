35adc9dae76717f459a48b75c5622a65d63778d2
Jeff Lee <jefferson0310@gmail.com>
2023-06-26 05:01:27 +0800

    Update get route

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../Models/UserModel");
 
 /* Import Controllers */
 const { Signup, Login } = require("../Controllers/AuthController");
@@ -9,5 +12,17 @@ const { userValidator, validate } = require("../Middlewares/AuthValidator");
 /* Post Routes */
 router.post("/signup", userValidator, validate, Signup);
 router.post("/login", Login);
+
+/* Get Routes */
+router.get("/is-auth", async (req, res) => {
+  const token = req.headers.authorization;
+  const jwtToken = token.split("Bearer ")[1];
+  if (!jwtToken) return sendError(res, "No token provided", 401);
+  const jwtRes = jwt.verify(jwtToken, process.env.TOKEN_KEY);
+  const { id } = jwtRes;
+  const user = await User.findById(id);
+  if (!user) return sendError(res, "User not found", 404);
+  res.status(200).json({ user });
+});
 
 module.exports = router;
