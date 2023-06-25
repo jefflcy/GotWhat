c499de521a101b9f66cd9570dacb587540074009
Jeff Lee <jefferson0310@gmail.com>
2023-06-26 03:57:38 +0800

    Implemented Backend to Frontend

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,40 @@
 import SideMenu from "../components/SideMenu";
+import { useState } from "react";
+import axios from "axios";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (input) => {
+    setEmail(input.target.value);
+  };
+
+  const handlePasswordChange = (input) => {
+    setPassword(input.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:4000/login", {
+        email,
+        password,
+      });
+
+      const token = response.data.token;
+
+      // Save the token in localStorage or a state management solution (e.g., Redux) for future use
+      console.log(token);
+
+      // Redirect the user to a protected page or perform other actions as needed
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="bg-[#365b6d] h-screen">
-     
       <SideMenu />
 
       <div className="flex flex-1 justify-center py-8 lg:px-8">
@@ -13,23 +44,20 @@ export default function LoginPage() {
       </div>
 
       <div className="mt-5 mx-auto w-full sm:max-w-sm shadow-xl bg-white rounded-lg p-4">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
-            <label
-              htmlFor="email"
-              className="block text-large font-bold leading-6 text-gray-900"
-            >
+            <label className="block text-large font-bold leading-6 text-gray-900">
               Email address
             </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="jefflee@hotmail.com"
-                autoComplete="email"
-                required
-                className="mt-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+            <input
+              type="email"
+              placeholder="jefflee@hotmail.com"
+              value={email}
+              onChange={handleEmailChange}
+              autoComplete="email"
+              required
+              className="mt-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
           </div>
 
           <div>
@@ -52,10 +80,10 @@ export default function LoginPage() {
 
             <div className="mt-2">
               <input
-                id="password"
-                name="password"
                 type="password"
                 placeholder="********"
+                value={password}
+                onChange={handlePasswordChange}
                 autoComplete="current-password"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
