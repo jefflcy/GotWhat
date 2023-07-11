1f5048721b55124061dfc1b1ac5e5c9c5d1c7c28
Jeff Lee <jefferson0310@gmail.com>
2023-07-12 04:34:23 +0800

    Code cleanup

--- server/Routes/SearchRoute.js
+++ server/Routes/SearchRoute.js
@@ -3,15 +3,6 @@ const router = require("express").Router();
 /* Import Controllers */
 const { Search } = require("../Controllers/SearchController");
 
-// Example search data (replace with your actual data source)
-/*
-const searchData = [
-  { id: 1, title: 'Result 1' },
-  { id: 2, title: 'Result 2' },
-  { id: 3, title: 'Result 3' },
-];
-*/
-
 // Search endpoint
 router.get("/search", Search);
 
