26086458ba550854e4a73c502f55b36f33c03c2d
aseah15 <e0958666@u.nus.edu>
2023-07-18 18:55:22 +0800

    created route and controller for menu and banner

--- server/Controllers/DashboardController.js
+++ server/Controllers/DashboardController.js
@@ -1,6 +1,5 @@
-const User = require("../Models/UserModel");
-const BizOwner = require("../Models/BizOwnerModel");
 const bcrypt = require("bcryptjs");
+const cloudinary = require('cloudinary').v2;
 
 
 module.exports.getUser = async (req, res) => {
@@ -39,3 +38,63 @@ module.exports.updatePassword = async (req, res) => {
         return res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+module.exports.updateMenu = async (req, res) => {
+    try {
+        const { path } = req.files.file;
+
+        const result = await cloudinary.uploader.upload(path);
+
+        const fileUrl = result.secure_url;
+
+        //save fileUrl to database
+
+        res.json({ fileUrl });
+    } catch (error) {
+        return res.status(500).json({ message: 'Failed to upload file' });
+    }
+};
+
+module.exports.updateMenuUrl = async (req, res) => {
+    try {
+        const { user } = req;
+        const { newMenuUrl } = req.body;
+
+        user.menuUrl = newMenuUrl;
+        await user.save();
+
+        res.json({ message: 'Menu updated successfully' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+module.exports.updateBanner = async (req, res) => {
+    try {
+        const { path } = req.files.file;
+
+        const result = await cloudinary.uploader.upload(path);
+
+        const fileUrl = result.secure_url;
+
+        //save fileUrl to database
+
+        res.json({ fileUrl });
+    } catch (error) {
+        return res.status(500).json({ message: 'Failed to upload file' });
+    }
+};
+
+module.exports.updateBannerUrl = async (req, res) => {
+    try {
+        const { user } = req;
+        const { newBannerUrl } = req.body;
+
+        user.bannerUrl = newBannerUrl;
+        await user.save();
+
+        res.json({ message: 'Banner updated successfully' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
--- server/Routes/DashboardRoute.js
+++ server/Routes/DashboardRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 //Import Controller
-const { getUser, updatePassword } = require('../Controllers/DashboardController');
+const { getUser, updatePassword, updateMenu, updateMenuUrl, updateBanner, updateBannerUrl } = require('../Controllers/DashboardController');
 
 //Import Middlewares
 const { validate } = require("../Middlewares/AuthLoginValidator");
@@ -9,7 +9,19 @@ const { validate } = require("../Middlewares/AuthLoginValidator");
 //Get Route to retrive user information
 router.get('/user', validate, getUser);
 
-//Post Route to update user password
+//Patch Route to update user password
 router.patch('/user/password', validate, updatePassword);
+
+//Patch Route to update/create PDF menu
+router.patch('/user/upload', validate, updateMenu);
+
+//Patch Route to update user menu Url
+router.patch('/user/menu', validate, updateMenuUrl);
+
+//Patch Route to update/create Img banner
+router.patch('/user/upload', validate, updateBanner);
+
+//Patch Route to update user banner Url
+router.patch('/user/menu', validate, updateBannerUrl);
 
 module.exports = router;
