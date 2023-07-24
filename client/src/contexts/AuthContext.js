3aaffe4be4892b512d8cbbe1c458924edd020c90
Jeff Lee <jefferson0310@gmail.com>
2023-07-24 09:04:43 +0800

    Added useEffect to check existing token validity

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,19 +1,37 @@
 import { createContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import axios from "axios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
   useEffect(() => {
-    const storedToken = localStorage.getItem("token") || Cookies.get("token");
-    if (storedToken) {
-      setToken(storedToken);
-      setIsAuthenticated(true);
-    }
-  }, []);
+    const checkTokenValidity = async () => {
+      const storedToken = localStorage.getItem("token") || Cookies.get("token");
+      try {
+        // Make a GET request to backend to check storedToken validity
+        const { data } = await axios.get(`${backendUrl}/isValidToken`, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        });
+
+        if (data.valid) {
+          setToken(storedToken);
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        // catching axios error here means storedToken is invalid/missing
+        //console.log(error);
+      }
+    };
+
+    checkTokenValidity();
+  }, [backendUrl]);
 
   const login = (token) => {
     setToken(token);
@@ -25,6 +43,7 @@ const AuthProvider = ({ children }) => {
     setToken(null);
     setIsAuthenticated(false);
     localStorage.removeItem("token");
+    Cookies.remove("token");
   };
 
   const authContextValue = {
